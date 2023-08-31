241c56ab7c1c62c1e6c0ebf21445753a1ead29e1
Andrew Drabek <drabekam@mail.uc.edu>
2023-08-30 21:32:56 -0400

    fixed reader code

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -2,83 +2,116 @@ import javax.swing.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 
 public class PersonReader {
 
     public static void main(String[] args) {
-            JFileChooser chooser = new JFileChooser();
-            File selectedFile;
-            String rec = "";
 
-            try
+
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> lines = new ArrayList<>();
+
+        /*
+
+        Here is the data file we are reading:
+        000001, Bilbo, Baggins, Esq., 1060
+        000002, Frodo, Baggins, Esq., 1120
+        000003, Samwise, Gamgee, Esq., 1125
+        000004, Peregrin, Took, Esq., 1126
+        000005, Meridoc, Brandybuck, Esq., 1126
+
+        */
+
+        final int FIELDS_LENGTH = 5;
+
+        String id, firstName, lastName, title;
+        int yob;
+
+        try
+        {
+
+            // use the toolkit to get the current working directory of the IDE
+            // Not sure if the toolkit is thread safe...
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
             {
-                // uses a fixed known path:
-                //  Path file = Paths.get("c:\\My Documents\\data.txt");
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
 
-                // use the toolkit to get the current working directory of the IDE
-                // Not sure if the toolkit is thread safe...
-                File workingDirectory = new File(System.getProperty("user.dir"));
+                System.out.println("ID#   Firstname       Lastname      Title     YOB");
+                System.out.println("======================================================");
 
-                // Typiacally, we want the user to pick the file so we use a file chooser
-                // kind of ugly code to make the chooser work with NIO.
-                // Because the chooser is part of Swing it should be thread safe.
-                chooser.setCurrentDirectory(workingDirectory);
-                // Using the chooser adds some complexity to the code.
-                // we have to code the complete program within the conditional return of
-                // the filechooser because the user can close it without picking a file
-
-                if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+                // Finally we can read the file LOL!
+                int line = 0;  // if we want to keep track of the line numbers
+                while(reader.ready())
                 {
-                    selectedFile = chooser.getSelectedFile();
-                    Path file = selectedFile.toPath();
-                    // Typical java pattern of inherited classes
-                    // we wrap a BufferedWriter around a lower level BufferedOutputStream
-                    InputStream in =
-                            new BufferedInputStream(Files.newInputStream(file, CREATE));
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(in));
-
-                    System.out.println("ID#           Firstname       Lastname      Title     YOB");
-                    System.out.println("======================================================");
-
-                    // Finally we can read the file LOL!
-                    int line = 0;
-                    while(reader.ready())
-                    {
-                        rec = reader.readLine();
-                        //line++;
-                        // echo to screen
-                        //System.out.printf("\nLine %4d %-60s ", rec);
-                        String[] parts = rec.split(",");
-                        if (parts.length >= 5) {
-                            String id = parts[0];
-                            String firstName = parts[1];
-                            String lastName = parts[2];
-                            String title = parts[3];
-                            String yob = parts[4];
-
-                            String formattedLine = String.format("%-10s%-15s%-15s%-10s%-6s",
-                                    id, firstName, lastName, title, yob);
-                            System.out.println(formattedLine);
-                        }
-
-
-                    }
-                    reader.close(); // must close the file to seal it and flush buffer
-                    System.out.println("\n\nData file read!");
+                    rec = reader.readLine();
+                    lines.add(rec);  // read all the lines into memory in an array list
+                    //line++;
+                    // echo to screen
+                    //System.out.printf("\nLine %4d %-60s ", line, rec);
                 }
+                reader.close(); // must close the file to seal it and flush buffer
+                //System.out.println("\n");
+
+                // Now process the lines in the arrayList
+                // Split the line into the fields by using split with a comma
+                // use trim to remove leading and trailing spaces
+                // Numbers need to be converted back to numberic values. Here only
+                // the last field year of birth yob is an int the rest are strings.
+
+                String[] fields;
+                for(String l:lines)
+                {
+                    fields = l.split(","); // Split the record into the fields
+
+                    if(fields.length == FIELDS_LENGTH)
+                    {
+                        id        = fields[0].trim();
+                        firstName = fields[1].trim();
+                        lastName  = fields[2].trim();
+                        title     = fields[3].trim();
+                        yob       = Integer.parseInt(fields[4].trim());
+                        System.out.printf("\n%-8s%-25s%-25s%-6s%6d", id, firstName, lastName, title, yob);
+                    }
+                    else
+                    {
+                        System.out.println("Found a record that may be corrupt: ");
+                        System.out.println(l);
+                    }
+                }
+
             }
-            catch (FileNotFoundException e)
+            else  // user closed the file dialog wihtout choosing
             {
-                System.out.println("File not found!!!");
-                e.printStackTrace();
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+        }  // end of TRY
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         }
 
 
